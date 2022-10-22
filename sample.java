c71c795ed63614eee235a6983cbd80fa0f3ae046
manojrc6 <114208616+manojrc6@users.noreply.github.com>
2022-10-22 13:11:28 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,10 @@
+public class HelloGuys{
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hello manoj");
+    System.out.println("Hello kumar");
+    System.out.println("Hello Ragu");
+    System.out.println("Hello Ravi");
+      }
+}
